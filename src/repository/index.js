5ded731c166ae0f6e2ec7f5e7d475ba35bdf285d
gaurav-shift <agarwalgaurav302005@gmail.com>
2026-02-06 12:20:46 +0530

    Added flight create crud

--- src/repository/index.js
+++ src/repository/index.js
@@ -1,5 +1,7 @@
 module.exports = {
-  CityRepository: require("./city_repository")
+  CityRepository: require("./city_repository"),
+  FlightRepository: require('./flight_repository'),
+  AirplaneRepository: require('./airplane_repository')
 }
 
 // UseCase of this index file is to export for a cleaner way of coding ! 
