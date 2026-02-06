7b2d971d4fe2d610653ec17381de3844188d5518
gaurav-shift <agarwalgaurav302005@gmail.com>
2026-02-06 13:42:49 +0530

    Added flight search crud with few filters

--- src/repository/flight_repository.js
+++ src/repository/flight_repository.js
@@ -1,6 +1,38 @@
 const {Flights} = require('../models/index');
+const   {Op} = require('sequelize');
 
 class FlightRepository{
+
+    #createFilter(data){
+        let filter = {};
+        if(data.arrivalAirportId){
+            filter.arrivalAirportId = data.arrivalAirportId;
+        }
+        if(data.departureId){
+            filter.departureAirportId = data.departureAirportId;
+        }
+        let priceFilter = [];
+        if(data.minPrice){
+            //Object.assign(filter,{price:{[Op.gte]:data.minPrice}});
+            priceFilter.push({price:{[Op.gte]:data.minPrice}});
+        }
+        if(data.maxPrice){
+            //Object.assign(filter,{price:{[Op.lte]:data.maxPrice}});
+            priceFilter.push({price:{[Op.lte]:data.maxPrice}});
+        }
+        Object.assign(filter,{[Op.and]:priceFilter});
+        // if(data.minPrice && data.maxPrice){
+        //     Object.assign(filter,{
+        //         [Op.and]:[
+        //             {price:{[Op.lte]:data.maxPrice}},
+        //             {price:{[Op.gte]:data.minPrice}}
+        //         ]
+        //     })
+        // }
+        return filter;
+    }
+
+
     async createFlight(data){
         try {
             const flight = await Flights.create(data);
@@ -10,6 +42,37 @@ class FlightRepository{
             throw {error};
         }
     }
+    async getFlight(flightId){
+        try {
+            const flight = await Flights.findByPk(data);
+            return flight;
+        } catch (error) {
+            console.log("Something went wrong in the repository layer");
+            throw {error};
+        }
+    }
+    async getAllFlights(filter){
+        try {
+            const filterObject = this.#createFilter(filter);
+            const flight = await Flights.findAll({
+                where: filterObject
+            });
+            return flight;
+        } catch (error) {
+            console.log("Something went wrong in the repository layer");
+            throw {error};
+        }
+    }
 }
 
 module.exports = FlightRepository; 
+
+/*
+{
+    where:{
+        arrivalAirportId: 2,
+        departureAirportId:4,
+        price:{[Op.gate]:4000} .// price is greaterthan=to 4000
+    }
+}
+*/
